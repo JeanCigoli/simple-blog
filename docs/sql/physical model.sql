6f54f75e4e3bdd2e901e7e5850a268456d3d9086
jeancigoli <jean@primi.com.br>
2021-12-18 01:27:45 -0300

    docs(sql): alter id on init the word to final

--- docs/sql/physical model.sql
+++ docs/sql/physical model.sql
@@ -1,5 +1,7 @@
 -- MySQL Workbench Forward Engineering
 
+DROP DATABASE blog;
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -18,7 +20,7 @@ USE `blog` ;
 -- Table `blog`.`tb_blog`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blog`.`tb_blog` (
-  `id_blog` INT NOT NULL AUTO_INCREMENT,
+  `blog_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(50) NOT NULL,
   `short_description` VARCHAR(200) NOT NULL,
   `author` VARCHAR(70) NOT NULL,
@@ -27,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `blog`.`tb_blog` (
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` DATETIME NOT NULL,
   `external_id` VARCHAR(36) NOT NULL,
-  PRIMARY KEY (`id_blog`))
+  PRIMARY KEY (`blog_id`))
 ENGINE = InnoDB;
 
 
@@ -35,11 +37,11 @@ ENGINE = InnoDB;
 -- Table `blog`.`tb_category`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blog`.`tb_category` (
-  `id_category` INT NOT NULL AUTO_INCREMENT,
+  `category_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `external_id` VARCHAR(36) NOT NULL,
-  PRIMARY KEY (`id_category`))
+  PRIMARY KEY (`category_id`))
 ENGINE = InnoDB;
 
 
@@ -47,20 +49,20 @@ ENGINE = InnoDB;
 -- Table `blog`.`tb_rel_blog_category`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blog`.`tb_rel_blog_category` (
-  `id_rel_blog_category` INT NOT NULL AUTO_INCREMENT,
-  `id_blog` INT NOT NULL,
-  `id_category` INT NOT NULL,
-  PRIMARY KEY (`id_rel_blog_category`),
-  INDEX `fk_tb_rel_blog_category_tb_blog_idx` (`id_blog` ASC) VISIBLE,
-  INDEX `fk_tb_rel_blog_category_tb_category1_idx` (`id_category` ASC) VISIBLE,
+  `rel_blog_category_id` INT NOT NULL AUTO_INCREMENT,
+  `blog_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`rel_blog_category_id`),
+  INDEX `fk_tb_rel_blog_category_tb_blog_idx` (`blog_id` ASC) VISIBLE,
+  INDEX `fk_tb_rel_blog_category_tb_category1_idx` (`category_id` ASC) VISIBLE,
   CONSTRAINT `fk_tb_rel_blog_category_tb_blog`
-    FOREIGN KEY (`id_blog`)
-    REFERENCES `blog`.`tb_blog` (`id_blog`)
+    FOREIGN KEY (`blog_id`)
+    REFERENCES `blog`.`tb_blog` (`blog_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tb_rel_blog_category_tb_category1`
-    FOREIGN KEY (`id_category`)
-    REFERENCES `blog`.`tb_category` (`id_category`)
+    FOREIGN KEY (`category_id`)
+    REFERENCES `blog`.`tb_category` (`category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -70,16 +72,16 @@ ENGINE = InnoDB;
 -- Table `blog`.`tb_section`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blog`.`tb_section` (
-  `id_section` INT NOT NULL AUTO_INCREMENT,
-  `id_blog` INT NOT NULL,
+  `section_id` INT NOT NULL AUTO_INCREMENT,
+  `blog_id` INT NOT NULL,
   `title` VARCHAR(70) NULL,
   `text` TEXT NOT NULL,
   `image` VARCHAR(200) NULL,
-  PRIMARY KEY (`id_section`),
-  INDEX `fk_tb_section_tb_blog1_idx` (`id_blog` ASC) VISIBLE,
+  PRIMARY KEY (`section_id`),
+  INDEX `fk_tb_section_tb_blog1_idx` (`blog_id` ASC) VISIBLE,
   CONSTRAINT `fk_tb_section_tb_blog1`
-    FOREIGN KEY (`id_blog`)
-    REFERENCES `blog`.`tb_blog` (`id_blog`)
+    FOREIGN KEY (`blog_id`)
+    REFERENCES `blog`.`tb_blog` (`blog_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
