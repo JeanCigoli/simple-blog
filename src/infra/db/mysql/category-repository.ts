b85bf319eea3f292ca5c88c8a6063cbcf9e8ed22
jeancigoli <jean@primi.com.br>
2021-12-18 02:22:35 -0300

    chore: add new methods in CategoryRepository

--- src/infra/db/mysql/category-repository.ts
+++ src/infra/db/mysql/category-repository.ts
@@ -1,8 +1,40 @@
-import { ListAllCategoriesRepository } from '@/data/protocols/db';
+import {
+  CreateCategoryRepository,
+  ListAllCategoriesRepository,
+  ListOneCategoryByNameRepository,
+} from '@/data/protocols/db';
+import {
+  formateCamelCaseKeysForSnakeCase,
+  formateSnakeCaseKeysForCamelCase,
+} from '@badass-team-code/formatted-cases-words';
 import { dbBlog } from './helper';
 
-export class CategoryRepository implements ListAllCategoriesRepository {
-  findAll(): ListAllCategoriesRepository.Result {
-    return dbBlog('blog.tb_category').select('*');
+export class CategoryRepository
+  implements
+    ListAllCategoriesRepository,
+    ListOneCategoryByNameRepository,
+    CreateCategoryRepository
+{
+  create(
+    params: CreateCategoryRepository.Params,
+  ): CreateCategoryRepository.Result {
+    return dbBlog('blog.tb_category').insert(
+      formateCamelCaseKeysForSnakeCase(params),
+    );
+  }
+
+  async findByName(name: string): ListOneCategoryByNameRepository.Result {
+    const category = await dbBlog('blog.tb_category')
+      .select('*')
+      .where('name', name)
+      .first();
+
+    return formateSnakeCaseKeysForCamelCase(category);
+  }
+
+  async findAll(): ListAllCategoriesRepository.Result {
+    const categories = await dbBlog('blog.tb_category').select('*');
+
+    return formateSnakeCaseKeysForCamelCase(categories);
   }
 }
