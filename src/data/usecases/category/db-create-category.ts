47e81dd7a0a41a5074bef36b5ecc3536e81d947b
jeancigoli <jean@primi.com.br>
2021-12-18 02:08:01 -0300

    chore: implement createCategory and add interface generate uuid

--- src/data/usecases/category/db-create-category.ts
+++ src/data/usecases/category/db-create-category.ts
@@ -1,7 +1,35 @@
+import {
+  CreateCategoryRepository,
+  ListOneCategoryByNameRepository,
+} from '@/data/protocols/db';
+import { GenerateUuid } from '@/data/protocols/utils';
 import { CreateCategory } from '@/domain/usecases';
 
 export class DbCreateCategory implements CreateCategory {
+  constructor(
+    private readonly listOneCategoryByNameRepository: ListOneCategoryByNameRepository,
+    private readonly createCategoryRepository: CreateCategoryRepository,
+    private readonly generateUuid: GenerateUuid,
+  ) {}
+
   async create(params: CreateCategory.Params): CreateCategory.Result {
-    return {} as CreateCategory.Result;
+    const categoryExist = await this.listOneCategoryByNameRepository.findByName(
+      params.name,
+    );
+
+    if (categoryExist) {
+      throw new Error('CATEGORY_EXIST');
+    }
+
+    const category = await this.createCategoryRepository.create({
+      createdAt: new Date(),
+      name: params.name,
+      externalId: this.generateUuid(),
+    });
+
+    return {
+      id: category.externalId,
+      name: category.name,
+    };
   }
 }
