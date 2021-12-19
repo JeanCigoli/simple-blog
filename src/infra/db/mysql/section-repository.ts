06ba3073d52acfb91eef3dcd09ca4318f664a9b8
jeancigoli <jean@primi.com.br>
2021-12-19 13:02:53 -0300

    chore: add new methods in SectionRepository

--- src/infra/db/mysql/section-repository.ts
+++ src/infra/db/mysql/section-repository.ts
@@ -1,13 +1,43 @@
 import {
   CreateSectionRepository,
   ListAllSectionByBlogRepository,
+  ListOneSectionByExternalIdRepository,
+  UpdateSectionRepository,
 } from '@/data/protocols/db';
-import { formateCamelCaseKeysForSnakeCase } from '@badass-team-code/formatted-cases-words';
+import {
+  formateCamelCaseKeysForSnakeCase,
+  formateSnakeCaseKeysForCamelCase,
+} from '@badass-team-code/formatted-cases-words';
 import { dbBlog } from './helper';
 
 export class SectionRepository
-  implements CreateSectionRepository, ListAllSectionByBlogRepository
+  implements
+    CreateSectionRepository,
+    ListAllSectionByBlogRepository,
+    ListOneSectionByExternalIdRepository,
+    UpdateSectionRepository
 {
+  update(
+    params: UpdateSectionRepository.Params,
+    id: number,
+  ): UpdateSectionRepository.Result {
+    return dbBlog('blog.tb_section')
+      .update(formateCamelCaseKeysForSnakeCase(params))
+      .where('section_id', id);
+  }
+
+  async findByExternalId(
+    params: ListOneSectionByExternalIdRepository.Params,
+  ): ListOneSectionByExternalIdRepository.Result {
+    const section = await dbBlog('blog.tb_section')
+      .select('*')
+      .where('external_id', params.sectionId)
+      .andWhere('blog_id', params.blogId)
+      .first();
+
+    return formateSnakeCaseKeysForCamelCase(section);
+  }
+
   findByBlog(id: number): ListAllSectionByBlogRepository.Result {
     return dbBlog('blog.tb_section')
       .select('title', 'text', 'image', 'external_id as id')
