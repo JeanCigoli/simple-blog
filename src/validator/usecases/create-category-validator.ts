06c0a948a114ccdff480b5978c0c15ca4d4f6ff7
jeancigoli <jean@primi.com.br>
2021-12-18 23:42:44 -0300

    refactor: improves category creation validation

--- src/validator/usecases/create-category-validator.ts
+++ src/validator/usecases/create-category-validator.ts
@@ -2,7 +2,9 @@ import * as yup from 'yup';
 import { string } from '..';
 
 const createCategorySchema = yup.object().shape({
-  name: string('A categoria').required('A categoria é obrigatória'),
+  name: string('A categoria')
+    .max(50, 'A categoria não pode ter mais de 50 caracteres')
+    .required('A categoria é obrigatória'),
 });
 
 export { createCategorySchema };
