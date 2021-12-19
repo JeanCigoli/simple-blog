b17a0c76fa2ca649d5aa99aa2827cc574bfe1458
jeancigoli <jean@primi.com.br>
2021-12-19 02:12:07 -0300

    chore: add ListBlogByIdController

---
+++ src/presentation/controller/blog/list-blog-by-id-controller.ts
@@ -0,0 +1,26 @@
+import { ListBlogById } from '@/domain/usecases';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '@/presentation/protocols';
+import { notFound, ok, serverError } from '@/utils/response';
+
+export class ListBlogByIdController implements Controller {
+  constructor(private readonly listBlogById: ListBlogById) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const blogs = await this.listBlogById.listById(httpRequest.query);
+
+      return ok('Listagem detalhada da publicação', blogs);
+    } catch (error: any) {
+      switch (error.message) {
+        case 'BLOG_NOT_FOUND':
+          return notFound('Nenhuma publicação com essa chave foi encontrada');
+        default:
+          return serverError(error);
+      }
+    }
+  }
+}
